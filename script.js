e5c23d7295fc0a00732b8070247e65797f8364e1
apal <alishapal96@gmail.com>
2021-06-29 21:13:29 -0700

    create displayQuestionAnswer function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("test")
 var timeCount = document.querySelector(".timer")
 var startButton = document.querySelector(".start")
+var q1text = document.querySelector(".question1")
 var timeLeft =75;
 var questionAnswers = {
     q1:"Objects are enclosed in: ",
@@ -22,8 +23,6 @@ var questionAnswers = {
         c:"Returns a pseudo-random number in the range 0 to less than 1 ",
         d:"Returns a pseudo-random number in the range 0 to infinity"}
 
-
-
 }
 
 function timer(){
@@ -41,16 +40,43 @@ function timer(){
     }
 },1000);
 };
- 
+function displayQuestionAnswer() {
+    var q1ElText = document.createElement("ol")
+    var AnswerA = document.createElement("li")
+    var AnswerB = document.createElement("li")
+    var AnswerC = document.createElement("li")
+    var AnswerD = document.createElement("li")
+    q1ElText.textContent = questionAnswers.q1;
+    AnswerA.textContent = questionAnswers.Answersq1.a;
+    AnswerB.textContent = questionAnswers.Answersq1.b;
+    AnswerC.textContent = questionAnswers.Answersq1.c;
+    AnswerD.textContent = questionAnswers.Answersq1.d;
+    
+
+    q1text.appendChild(q1ElText)
+    q1ElText.appendChild(AnswerA)
+    q1ElText.appendChild(AnswerB)
+    q1ElText.appendChild(AnswerC)
+    q1ElText.appendChild(AnswerD)
+
+    listAnswer.textContent = Answerq1A;
+    console.log(Answerq1A)
+};
+
 function startQuiz(){
+    startButton.setAttribute("style","display:none");
+
+
+
+
+
+
     console.log("Hello ")
     timer();
-    clearInterval(startButton);
+    
 
 };
 
 
-startButton.addEventListener("click",startQuiz);
-{
 
-}
+startButton.addEventListener("click",startQuiz);
