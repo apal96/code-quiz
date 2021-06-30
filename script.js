63d65104902e7fe5244ce35af98f06a4315d5d23
apal <alishapal96@gmail.com>
2021-06-29 19:31:53 -0700

    add timer function

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("test")
+var timeCount = document.querySelector(".timer")
+var startButton = document.querySelector(".start")
+var timeLeft =75;
+
+function timer(){
+    var timerCount = setInterval(function(){
+    
+    if (timeLeft >1){
+        console.log(timeLeft)
+        timeCount.textContent = timeLeft;
+        timeLeft--;
+       
+    }else if (timeLeft===0){
+        console.log()
+        
+    
+    }
+},1000);
+};
+ 
+function startQuiz(){
+    console.log("Hello ")
+    timer();
+};
+
+
+startButton.addEventListener("click",startQuiz);
